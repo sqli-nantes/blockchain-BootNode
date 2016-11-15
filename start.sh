ea908925ded343cb67feaeeedf5ccb913e439b09
GABORIEAU Romain <rgaborieau@sqli.com>
2016-11-15 10:39:11 +0100

    Test de fix pour devfest

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export GETH='/home/geth/geth-1.4.5-stable-a269a71-linux-amd64
 	--rpcaddr 0.0.0.0 
 	--rpcport 8547
 	--rpcapi admin,eth,miner,net,web3,personal
-	--rpccorsdomain "http://localhost:8547,http://localhost:8080"
+	--rpccorsdomain http://localhost:8547,http://localhost:8080
 	--rpc 
 	--exec "loadScript("sendMoney.js")" 
 	--nodiscover 
@@ -28,10 +28,12 @@ $GETH --password $GETH_DIR/password js <(echo 'miner.start();admin.sleepBlocks(1
 sed -i -- 's#\[::\]#'$( hostname --ip-address )'#g' $HTTPD_DIR/current.json 
 
 
-$GETH --exec 'loadScript("'$GETH_DIR'/MineOnlyWhenTx.js")' &
+$GETH &
 
 sleep 10
 $GETH --exec 'admin.nodeInfo.enode' attach ipc:$GETH_DIR/geth.ipc > /home/enode.txt
+
+#$GETH --exec 'loadScript("'$GETH_DIR'/MineOnlyWhenTx.js")' attach ipc:$GETH_DIR/geth.ipc > /home/mineonly.log
 
 #while grep "Fatal" /home/enode.txt >/dev/null 2>&1 ; do
 #	$GETH  attach ipc:$GETH_DIR/geth.ipc > /home/enode.txt
@@ -41,12 +43,14 @@ $GETH --exec 'admin.nodeInfo.enode' attach ipc:$GETH_DIR/geth.ipc > /home/enode.
 echo "================= enode ======================" 
 cat /home/enode.txt
 
-/usr/bin/geth --exec 'eth.coinbase' attach $GETH_DIR/geth.ipc > /home/coinbase.txt
+#$GETH --exec 'eth.coinbase' attach ipc:$GETH_DIR/geth.ipc > /home/coinbase.txt
 
-sed -i -- 's/"//g' /home/coinbase.txt
-echo ">"$(cat /home/coinbase.txt)"<"
-coinbase=$(cat /home/coinbase.txt | awk '{$1=$1};1')
-wget -qO- http://10.33.44.212:8081/names\?name\="Wallet"\&address\=$coinbase &> /dev/null
+#sed -i -- 's/"//g' /home/coinbase.txt
+#echo ">"$(cat /home/coinbase.txt)"<"
+#coinbase=$(cat /home/coinbase.txt | awk '{$1=$1};1')
+#wget -qO- http://10.33.44.212:8081/names\?name\="Wallet"\&address\=$coinbase &> /home/wallet.log
+
+$GETH --preload $GETH_DIR"/MineOnlyWhenTx.js" attach ipc:$GETH_DIR/geth.ipc
 
 httpd-foreground 
 
